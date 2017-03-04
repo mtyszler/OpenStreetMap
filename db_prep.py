c9e07d51f6424a1cf059459e58fec9d57662601c
mtyszler <tyszler.jobs@gmail.com>
2017-03-04 16:33:07 +0100

    Fix db_prep
    changed ,"w", to "wb" in dicwriter to avoid blank lines

--- db_prep.py
+++ db_prep.py
@@ -310,11 +310,11 @@ class UnicodeDictWriter(csv.DictWriter, object):
 def process_map(file_in, validate):
     """Iteratively process each XML element and write to csv(s)"""
 
-    with codecs.open(NODES_PATH, 'w') as nodes_file, \
-            codecs.open(NODE_TAGS_PATH, 'w') as nodes_tags_file, \
-            codecs.open(WAYS_PATH, 'w') as ways_file, \
-            codecs.open(WAY_NODES_PATH, 'w') as way_nodes_file, \
-            codecs.open(WAY_TAGS_PATH, 'w') as way_tags_file:
+    with codecs.open(NODES_PATH, 'wb') as nodes_file, \
+            codecs.open(NODE_TAGS_PATH, 'wb') as nodes_tags_file, \
+            codecs.open(WAYS_PATH, 'wb') as ways_file, \
+            codecs.open(WAY_NODES_PATH, 'wb') as way_nodes_file, \
+            codecs.open(WAY_TAGS_PATH, 'wb') as way_tags_file:
 
         nodes_writer = UnicodeDictWriter(nodes_file, NODE_FIELDS)
         node_tags_writer = UnicodeDictWriter(nodes_tags_file, NODE_TAGS_FIELDS)
