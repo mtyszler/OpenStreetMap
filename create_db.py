a1da2aa1b6713d04b3e5464774f0d6f21d5ca44b
mtyszler <tyszler.jobs@gmail.com>
2017-04-28 16:37:42 +0200

    Add update for the postcode

--- create_db.py
+++ create_db.py
@@ -25,7 +25,9 @@ def executeScriptsFromFile(filename):
 
 # Create a new empty db
 db = sqlite3.connect("OpenStreeMap_Nieuwegein.sqlite")
+db.text_factory = str
 c = db.cursor()
+
 
 # Create all tables
 executeScriptsFromFile("data_wrangling_schema.sql")
@@ -37,8 +39,26 @@ for part in data_source:
     print "Importing " + part
     df = pandas.read_csv(part + '.csv')
     df.to_sql(part, db, if_exists='append', index=False)
-
-
-
 db.commit()
+
+
+# Fix postcodes
+# Create a list of the closest full postcode. based on the lat, lon distance:
+sql = executeScriptsFromFile('support_file_for_post_codes.sql')
+results = c.fetchall()
+#print results
+# results: id, problem_postcode, best_match_postcode
+for problem_postcode in results:
+    print "fixing " +  str(problem_postcode[1]) +" with " + str(problem_postcode[2])
+    sql = "UPDATE nodes_tags" + \
+          " SET value = '" + str(problem_postcode[2]) + "'" \
+          " WHERE id = " + str(problem_postcode[0]) + \
+          " AND key = 'postcode'"
+
+    db.execute(sql)
+    db.commit()
+
+
+
+
 db.close()
